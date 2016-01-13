384934f32e50f09be4227364398ce28e86aa0099
ben <benben198805@126.com>
2016-01-13 15:12:24 +0800

    refactor switch L and R into location.trurnLeft and turnRight

--- src/main/java/Location.java
+++ src/main/java/Location.java
@@ -1,23 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by ben on 16-1-13.
  */
 public class Location {
-    private int LocationX;
-    private int LocationY;
+    private int locationX;
+    private int locationY;
     private String orientation;
+    private List<String> direction;
 
     public Location(int locationX, int locationY, String orientation) {
-        this.LocationX = locationX;
-        this.LocationY = locationY;
+        this.locationX = locationX;
+        this.locationY = locationY;
         this.orientation = orientation;
+        direction = new ArrayList<String>();
+        direction.add("W");
+        direction.add("N");
+        direction.add("E");
+        direction.add("S");
     }
 
     public int getLocationX() {
-        return LocationX;
+        return locationX;
     }
 
     public int getLocationY() {
-        return LocationY;
+        return locationY;
     }
 
     public String getOrientation() {
@@ -25,11 +34,11 @@ public class Location {
     }
 
     public void setLocationX(int locationX) {
-        LocationX = locationX;
+        this.locationX = locationX;
     }
 
     public void setLocationY(int locationY) {
-        LocationY = locationY;
+        this.locationY = locationY;
     }
 
     public void setOrientation(String orientation) {
@@ -38,7 +47,50 @@ public class Location {
 
     public String ShowLocation()
     {
-        String result=String.valueOf(LocationX)+" "+String.valueOf(LocationY)+" "+orientation;
+        String result=String.valueOf(locationX)+" "+String.valueOf(locationY)+" "+orientation;
         return result;
+    }
+
+    public void move() {
+        switch (this.orientation)
+        {
+            case "N":
+                    this.locationY++;
+                break;
+
+            case "S":
+                    this.locationY--;
+                break;
+
+            case "W":
+                    this.locationX--;
+                break;
+
+            case "E":
+                    this.locationX++;
+                break;
+        }
+    }
+
+    public void turnLeft() {
+        if(direction.indexOf(orientation)==0)
+        {
+            orientation=direction.get(direction.size()-1);
+        }
+        else
+        {
+            orientation=direction.get(direction.indexOf(orientation)-1);
+        }
+    }
+
+    public void turnRight() {
+        if(direction.indexOf(orientation)==direction.size()-1)
+        {
+            orientation=direction.get(0);
+        }
+        else
+        {
+            orientation=direction.get(direction.indexOf(orientation)+1);
+        }
     }
 }
